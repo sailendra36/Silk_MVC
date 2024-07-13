51216ea092587e49087039caaf42120a33be1948
sailendk <sailendk@DVCGNH6114857>
2024-07-13 12:16:40 +0530

    Add project files.

---
+++ SilkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SilkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
