de4ae56541ed511a2112501522956efe10a38f27
sailendra36 <sailendra00@gmail.com>
2024-07-20 12:19:47 +0530

    #2 Category Create,it's view/UI,client and server side Validations

--- SilkWeb/Controllers/CategoryController.cs
+++ SilkWeb/Controllers/CategoryController.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SilkWeb.Data;
+using SilkWeb.Models;
 
 namespace SilkWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (obj.Name!=null && obj.Name.ToString() == "test")
+            {
+                ModelState.AddModelError("", "test is invalid Name");       //asp-validation-summary="ModelOnly" will display this but not above error bind with property name 
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
             return View();
         }
     }
--- SilkWeb/Migrations/20240713154512_seedCategoryTable.Designer.cs
+++ SilkWeb/Migrations/20240713154512_seedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SilkWeb.Data;
 
@@ -10,9 +11,11 @@ using SilkWeb.Data;
 namespace SilkWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240713154512_seedCategoryTable")]
+    partial class seedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
